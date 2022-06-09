60772ed689c4844fdab0897edfe5923fd545bf7c
Christian AS <alvitesscf18@uww.edu>
2022-06-08 19:14:21 -0500

    began app UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css'
+import React, { useState } from 'react'
+import './App.scss'
+import InputField from './Components/InputField'
 
-function App() {
-	return <div className='App'></div>
+const App: React.FC = () => {
+	const [toDo, setToDo] = useState<string>('')
+	return (
+		<div className='App'>
+			<span className='App-heading'>Taskify</span>
+			<InputField />
+		</div>
+	)
 }
-
 export default App
